56f58234a8f44ce39c6f8c3bfffb8b29a73876e3
Hari Setyo Permadi <harypermadi40@gmail.com>
2021-08-10 12:26:26 +0700

    File Awal 10 agustus 2021 12:26

---
+++ app/src/main/java/com/harysetyopermadi/loginwithsqlite/logindanregis/Registrasi.java
@@ -0,0 +1,16 @@
+package com.harysetyopermadi.loginwithsqlite.logindanregis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.harysetyopermadi.loginwithsqlite.R;
+
+public class Registrasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+    }
+}
